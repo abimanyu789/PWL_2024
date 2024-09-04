88262f70e01d72ed5829c510e42798de3dec522e
abimanyu789 <anandaabimanyu7@gmail.com>
2024-09-04 10:59:13 +0700

    Js2 - praktikum2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,35 +22,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-   });
+Route::get('/hello',  [WelcomeController::class,'hello']);
 
-Route::get('/world', function () {
-    return 'World';
-   });
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-   });
+Route::get('/', [HomeController::class,'index']);
 
-Route::get('/about', function () {
-    return '2241760093: Ananda Abimanyu Saputra';
-   });
+Route::get('/about', [AboutController::class,'about']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
     });
-
+Route::get('/world', function () {
+        return 'World';
+       });
 Route::get('/posts/{post}/comments/{comment}', function
     ($postId, $commentId) {
      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
     });
 
-Route::get('/articles/{id}', function($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-    });
-
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+   ]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
